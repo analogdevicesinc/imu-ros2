d03722f52456d91ff7a1eb054cfd29b6c03659cd
Vasile Holonec <Vasile.Holonec@analog.com>
2023-02-02 15:16:35 +0200

    Added gyroscope test
    Signed-off-by: Vasile Holonec <Vasile.Holonec@analog.com>

---
+++ test/src/gyroscope_subscriber_test.cpp
@@ -0,0 +1,59 @@
+#include <rclcpp/rclcpp.hpp>
+#include <gtest/gtest.h>
+#include <chrono>
+#include "imu_ros2/msg/gyroscope_data.hpp"
+
+class GyroscopeSubscriberTest  : public ::testing::Test
+{
+public:
+    static void SetUpTestCase()
+    {
+
+    }
+
+    static void TearDownTestCase()
+    {
+        rclcpp::shutdown();
+    }
+};
+
+TEST(GyroscopeSubscriberTest,test_gyroscope_pozitive_values1)
+{
+    auto node = rclcpp::Node::make_shared("imugyroscope");
+
+    std::string topic = "imugyroscope";
+
+    int counter = 0;
+
+
+    auto callback =
+            [&counter]( imu_ros2::msg::GyroscopeData msg) -> void
+    {
+        ++counter;
+
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp_test_gyroscope_data"), " gyroscope value : %f  \n",
+                    msg.gyroscope_value );
+        ASSERT_TRUE(msg.gyroscope_value >= 0);
+
+
+
+    };
+
+    rclcpp::executors::SingleThreadedExecutor executor;
+    executor.add_node(node);
+
+    auto subscriber = node->create_subscription<imu_ros2::msg::GyroscopeData>(topic, 10, callback);
+
+    std::chrono::seconds sec(1);
+
+
+    for(int i=0;i<100;i++)
+    {
+    executor.spin_once(sec);
+    }
+
+    //executor.spin();
+
+    ASSERT_TRUE(true);
+}
+
