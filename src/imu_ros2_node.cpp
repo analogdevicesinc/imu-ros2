4497ef8ff2a2dae473c74f72a0a96a4f82f2851e
Vasile Holonec <Vasile.Holonec@analog.com>
2023-02-13 10:07:50 +0200

    Created minimalparam class
    Signed-off-by: Vasile Holonec <Vasile.Holonec@analog.com>

--- src/imu_ros2_node.cpp
+++ src/imu_ros2_node.cpp
@@ -22,65 +22,70 @@
 
 using namespace std::chrono_literals;
 
-void declareParameters(std::shared_ptr<rclcpp::Node>& node)
+class MinimalParam : public rclcpp::Node
 {
-    node->declare_parameter("my_parameter", "world");
-    node->declare_parameter("diag_checksum_error_flag", "val");
-    node->declare_parameter("diag_flash_memory_write_count_exceeded_error", "val");
-    node->declare_parameter("diag_acceleration_self_test_error", "val");
-    node->declare_parameter("diag_gyroscope2_self_test_error", "val");
-    node->declare_parameter("diag_gyroscope1_self_test_error", "val");
-    node->declare_parameter("diag_clock_error", "val");
-    node->declare_parameter("diag_flash_memory_test_error", "val");
-    node->declare_parameter("diag_sensor_self_test_error", "val");
-    node->declare_parameter("diag_standby_mode", "val");
-    node->declare_parameter("diag_spi_communication_error", "val");
-    node->declare_parameter("diag_flash_memory_update_error", "val");
-    node->declare_parameter("diag_data_path_overrun", "val");
-    node->declare_parameter("time_stamp", "val");
-    node->declare_parameter("data_counter", "val");
-    node->declare_parameter("filter_size", "val");
-    node->declare_parameter("gyroscope_measurement_range", "val");
-    node->declare_parameter("burst_size_selection", "val");
-    node->declare_parameter("burst_data_selection", "val");
-    node->declare_parameter("linear_acceleration_compensation", "val");
-    node->declare_parameter("point_of_percussion_alignment", "val");
-    node->declare_parameter("internal_sensor_bandwidth", "val");
-    node->declare_parameter("sync_mode_select", "val");
-    node->declare_parameter("sync_polarity", "val");
-    node->declare_parameter("data_ready_polarity", "val");
-    node->declare_parameter("sync_signal_scale", "val");
-    node->declare_parameter("decimation_filter", "val");
-    node->declare_parameter("software_reset", "val");
-    node->declare_parameter("flash_memory_test", "val");
-    node->declare_parameter("flash_memory_update", "val");
-    node->declare_parameter("sensor_self_test", "val");
-    node->declare_parameter("factory_calibration_restore", "val");
-    node->declare_parameter("firmware_revison", "val");
-    node->declare_parameter("firmware_date", "val");
-    node->declare_parameter("product_id", "val");
-    node->declare_parameter("serial_number", "val");
-    node->declare_parameter("scratch_pad_register1", "val");
-    node->declare_parameter("scratch_pad_register2", "val");
-    node->declare_parameter("scratch_pad_register3", "val");
-    node->declare_parameter("flash_counter", "val");
-    node->declare_parameter("accel_calibbias_x", "val");
-    node->declare_parameter("accel_calibbias_y", "val");
-    node->declare_parameter("accel_calibbias_z", "val");
-    node->declare_parameter("gyro_calibbias_x", "val");
-    node->declare_parameter("gyro_calibbias_y", "val");
-    node->declare_parameter("gyro_calibbias_z", "val");
+public:
+  MinimalParam()
+  : Node("imu_ros2_node")
+  {
+      declare_parameter("my_parameter", "world");
+      declare_parameter("diag_checksum_error_flag", "val");
+      declare_parameter("diag_flash_memory_write_count_exceeded_error", "val");
+      declare_parameter("diag_acceleration_self_test_error", "val");
+      declare_parameter("diag_gyroscope2_self_test_error", "val");
+      declare_parameter("diag_gyroscope1_self_test_error", "val");
+      declare_parameter("diag_clock_error", "val");
+      declare_parameter("diag_flash_memory_test_error", "val");
+      declare_parameter("diag_sensor_self_test_error", "val");
+      declare_parameter("diag_standby_mode", "val");
+      declare_parameter("diag_spi_communication_error", "val");
+      declare_parameter("diag_flash_memory_update_error", "val");
+      declare_parameter("diag_data_path_overrun", "val");
+      declare_parameter("time_stamp", "val");
+      declare_parameter("data_counter", "val");
+      declare_parameter("filter_size", "val");
+      declare_parameter("gyroscope_measurement_range", "val");
+      declare_parameter("burst_size_selection", "val");
+      declare_parameter("burst_data_selection", "val");
+      declare_parameter("linear_acceleration_compensation", "val");
+      declare_parameter("point_of_percussion_alignment", "val");
+      declare_parameter("internal_sensor_bandwidth", "val");
+      declare_parameter("sync_mode_select", "val");
+      declare_parameter("sync_polarity", "val");
+      declare_parameter("data_ready_polarity", "val");
+      declare_parameter("sync_signal_scale", "val");
+      declare_parameter("decimation_filter", "val");
+      declare_parameter("software_reset", "val");
+      declare_parameter("flash_memory_test", "val");
+      declare_parameter("flash_memory_update", "val");
+      declare_parameter("sensor_self_test", "val");
+      declare_parameter("factory_calibration_restore", "val");
+      declare_parameter("firmware_revison", "val");
+      declare_parameter("firmware_date", "val");
+      declare_parameter("product_id", "val");
+      declare_parameter("serial_number", "val");
+      declare_parameter("scratch_pad_register1", "val");
+      declare_parameter("scratch_pad_register2", "val");
+      declare_parameter("scratch_pad_register3", "val");
+      declare_parameter("flash_counter", "val");
+      declare_parameter("accel_calibbias_x", "val");
+      declare_parameter("accel_calibbias_y", "val");
+      declare_parameter("accel_calibbias_z", "val");
+      declare_parameter("gyro_calibbias_x", "val");
+      declare_parameter("gyro_calibbias_y", "val");
+      declare_parameter("gyro_calibbias_z", "val");
 
 
-}
+  }
+
+};
+
 
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
 
-    std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("imu_ros2_node");
-
-    declareParameters(node);
+    std::shared_ptr<rclcpp::Node> node = std::make_shared<MinimalParam>();
 
     DataProviderInterface* dataStr = new DataProviderString();
     RosPublisherInterface * publisher1 = new RosPublisher1(node);
